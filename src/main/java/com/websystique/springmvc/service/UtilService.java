3667d4724e582155aead97ef62a7a286602163ec
Tejeshincedo <tejesh.l@incedoinc.com>
2018-02-09 14:29:10 +0530

    check ip method implemented

--- src/main/java/com/websystique/springmvc/service/UtilService.java
+++ src/main/java/com/websystique/springmvc/service/UtilService.java
@@ -5,4 +5,9 @@ public class UtilService {
 	public static void main(String[] args) {
 		System.out.println("Static Check method need to implement");
 	}
+	public String checkip()
+	{
+		return "10.100.212.50";
+	}
 }
+
