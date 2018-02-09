90dad5840ed9721ef6fa82dc970b0e69fca0ba2d
IBTS\ninganagouda.patil <ninganagouda.patil@incedoinc.com>
2018-02-09 12:47:33 +0530

    tejesh please implememt date method logic

---
+++ src/main/java/com/websystique/springmvc/service/UtilService.java
@@ -0,0 +1,8 @@
+package com.websystique.springmvc.service;
+
+public class UtilService {
+
+	public static void main(String[] args) {
+		System.out.println("Static Check method need to implement");
+	}
+}
